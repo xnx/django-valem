cf53b0e4beecd518f8b8225231500465388159c2
Martin Hanicinec <hanicinecm@gmail.com>
2022-01-26 09:53:37 +0100

    add coverage to the setup.py requirements

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "pyvalem>=2.4.0",
         "django-pyref>=0.5.1",
     ],
-    extras_require={"dev": ["black", "django==3", "ipython"]},
+    extras_require={"dev": ["black", "coverage", "django==3", "ipython"]},
     project_urls={
         "Bug Reports": "https://github.com/xnx/django-valem/issues",
     },
